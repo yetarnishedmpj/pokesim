9392b4b51144f24df5bad325dff9197a7c452d82
yetarnishedmpj <maharshij094@gmail.com>
2026-04-19 02:43:09 +0530

    fix: production build and types

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    cssMinify: true,
+    cssMinify: false,
   },
   server: {
     port: 5173,
